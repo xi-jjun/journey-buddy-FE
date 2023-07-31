40b89114f60b5bb1360f12ca52d13d8598e8dc70
Kim Jaejun <rlawowns97@gmail.com>
2023-08-01 02:15:41 +0900

    JB-FE-1 : 메인 페이지 markup 작업 완료
    - MainLayout에 처음 만들다가, pages 하위에 index.vue에 markup 작업물 이동

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+	<div>
+		<MainLayout>
+		</MainLayout>
+	</div>
 </template>
+<script setup>
+import MainLayout from "~/layouts/MainLayout.vue";
+</script>
+
+<style>
+* {
+  margin: 0px;
+  padding: 0px;
+}
+</style>
