ca3d8d1d1c5e6b52daf0ab8a46edfea0212e2d60
Kim Jaejun <rlawowns97@gmail.com>
2023-08-05 01:15:07 +0900

    JB-FE-3 : 채팅목록 화면 보여주기 위한 화면 markup 작업완료

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  devServer: {
-    port: 5001
-  }
+    app: {
+        head: {
+            charset: 'utf-8',
+            title: 'Journey-Buddy',
+            meta: [
+                {charset: 'utf-8'},
+                {name: 'viewport', content: 'width=device-width, initial-scale=1.0, user-scalable=no'}
+            ]
+        }
+    },
+    devtools: {enabled: false},
+    devServer: {
+        port: 5001
+    }
 })
