4cbf308bbc7aaab5b29db61d741ff6f4ffd16c01
Kim Jaejun <rlawowns97@gmail.com>
2023-08-06 22:40:08 +0900

    JB-FE-5 : 여행 히스토리 목록 페이지 markup 작업완료

---
+++ pages/journey-history/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<section class="journey-history-page">
+		<BackNavbarView :navbar-title="`AI와 함께한 여행 기록`">
+
+    </BackNavbarView>
+
+    <section class="journey-history-list-wrapper">
+			<JourneyCardView :journey-info-component="journeyComponent" v-for="journeyComponent in journeyList">
+
+      </JourneyCardView>
+    </section>
+  </section>
+</template>
+
+<script setup>
+import journeyApi from "~/service/journeyApi";
+
+let journeyList = [];
+journeyApi.all(response => {
+	journeyList = response.journeyList;
+});
+
+</script>
+<style scoped lang="css">
+.journey-history-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.journey-history-list-wrapper {
+  margin: 20px 12px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 8px;
+  grid-row-gap: 12px;
+  text-align: center;
+  overflow-y: scroll;
+}
+
+</style>
