06ce9a1bbed820d6aab4ef99a83837237071f5da
Kim Jaejun <rlawowns97@gmail.com>
2023-08-12 03:17:41 +0900

    JB-FE-7 : 사용자 상세정보 받아오는 api 연동

---
+++ service/userApi.js
@@ -0,0 +1,17 @@
+const userApi = {
+	async getUserDetail(userId, token) {
+		const config = useRuntimeConfig();
+		const { code, user } = await $fetch(`/api/v1/users/${userId}`, {
+			baseURL: config.public.API_BASE_URL,
+			headers: { Authorization: token },
+		});
+
+		if (code === 200) {
+			return user;
+		}
+
+		return {};
+	},
+}
+
+export default userApi;
