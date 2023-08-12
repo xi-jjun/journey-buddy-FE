edc9b86cfcf814548a126b1763ed57ba664e030b
Kim Jaejun <rlawowns97@gmail.com>
2023-08-12 22:11:08 +0900

    JB-FE-9 : 유저의 성향정보를 가져오는 api 추가

---
+++ service/userPersonalityTestingApi.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const userPersonalityTestingApi = {
+	async getUserPersonalityTestResult(userId, token) {
+		const config = useRuntimeConfig();
+		try {
+			const { data } = await axios.get(`${config.public.API_BASE_URL}/api/v1/users/${userId}/personalities`, {
+				headers: { Authorization: token, }
+			});
+			return data;
+		} catch (error) {
+			console.log("fail getUserPersonalityTestResult by ", error);
+			return error.response.data;
+		}
+	},
+}
+
+export default userPersonalityTestingApi;
