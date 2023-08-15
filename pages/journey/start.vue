606e0f0c4870111e6aaa03534fef2e16db8df9c6
Kim Jaejun <rlawowns97@gmail.com>
2023-08-15 17:02:10 +0900

    JB-FE-12 : 동행AI상세정보 가져오는 api 연동
    - onMounted로 하면 가져온 정보가 화면에 반영이 안돼서 제거

--- pages/journey/start.vue
+++ pages/journey/start.vue
@@ -1,8 +1,21 @@
 <script setup>
-const buddyName = '킴AI';
+import journeyApi from "~/service/journeyApi";
+import nuxtStorage from "nuxt-storage";
+import constant from "~/service/constant";
+
+const route = useRoute();
+const userTokenFromLocalStorage = nuxtStorage.localStorage.getData(constant.LOCAL_STORAGE_USER_TOKEN_KEY);
+
+let buddyDetailInfo = ref(null);
+const journeyId = route.query.journeyId;
+const result = await journeyApi.getBuddyDetailByJourneyId(journeyId, userTokenFromLocalStorage);
+if (result.code !== 200) {
+	console.log("error response from getBuddyDetailByJourneyId");
+} else {
+	buddyDetailInfo = result.buddy;
+}
 
 const touchScreen = () => {
-	const journeyId = 1;
 	navigateTo(`/chat?journey_id=${journeyId}`);
 };
 
@@ -12,9 +25,11 @@ const touchScreen = () => {
   <section class="journey-start-page" @click="touchScreen">
 		<img src="/images/login/jb_logo_login_page.svg" alt="journey-buddy logo" class="journey-buddy-logo"/>
 
-		<img src="/images/sign-up/profile_default_img_icon_sign_up_page.svg" alt="ai buddy profile image" class="ai-buddy-profile-image"/>
+		<img v-if="buddyDetailInfo" :src="buddyDetailInfo['profile_image_url']" alt="ai buddy profile image" class="ai-buddy-profile-image"/>
+		<img v-if="!buddyDetailInfo" src="/images/sign-up/profile_default_img_icon_sign_up_page.svg" alt="ai buddy profile image" class="ai-buddy-profile-image"/>
 
-    <span class="start-journey">{{ buddyName }}와 여행을 시작합니다!</span>
+    <span v-if="buddyDetailInfo" class="start-journey">{{ buddyDetailInfo['display_name'] }}와 여행을 시작합니다!</span>
+    <span v-if="!buddyDetailInfo" class="start-journey">AI와 여행을 시작합니다!</span>
     <span class="have-great-time">즐거운 여행 되세요!</span>
   </section>
 </template>
