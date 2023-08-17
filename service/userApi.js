6de19419aa58eb7812b35dfd23f28dbb14e9ea9d
Kim Jaejun <rlawowns97@gmail.com>
2023-08-18 03:49:47 +0900

    JB-FE-16 : 여행 히스토리 상세정보를 위한 api 추가

--- service/userApi.js
+++ service/userApi.js
@@ -27,6 +27,18 @@ const userApi = {
 			return error.response.data;
 		}
 	},
+	async getUserJourneyDetailInfo(userId, journeyId, token) {
+		const config = useRuntimeConfig();
+		try {
+			const { data } = await axios.get(`${config.public.API_BASE_URL}/api/v1/users/${userId}/journeys/${journeyId}`, {
+				headers: { Authorization: token, 'Access-Control-Allow-Origin': '*', 'ngrok-skip-browser-warning': '123' },
+			});
+			return data;
+		} catch (error) {
+			console.log("fail getUserJourneyDetailInfo by ", error);
+			return error.response.data;
+		}
+	},
 }
 
 export default userApi;
