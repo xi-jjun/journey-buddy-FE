c4ccd9cba4fbbe7f93fc2ff883fed40578ad815f
Kim Jaejun <rlawowns97@gmail.com>
2023-08-18 01:49:59 +0900

    JB-FE-16 : 여행 히스토리 목록 api 연동

--- service/userApi.js
+++ service/userApi.js
@@ -15,6 +15,18 @@ const userApi = {
 			return error.response.data;
 		}
 	},
+	async getUserJourneys(userId, token) {
+		const config = useRuntimeConfig();
+		try {
+			const { data } = await axios.get(`${config.public.API_BASE_URL}/api/v1/users/${userId}/journeys`, {
+				headers: { Authorization: token, 'Access-Control-Allow-Origin': '*', 'ngrok-skip-browser-warning': '123' },
+			});
+			return data;
+		} catch (error) {
+			console.log("fail getUserJourneys by ", error);
+			return error.response.data;
+		}
+	},
 }
 
 export default userApi;
