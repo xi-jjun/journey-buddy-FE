792333b3cc3fb28e1c65df98f2aad1bca7c417cb
Kim Jaejun <rlawowns97@gmail.com>
2023-08-26 21:00:17 +0900

    JB-FE-26 : 유저정보 수정 페이지 데이터 연동
    - 기존 정보들이 항목에 추가되어 있도록 수정
    - 비밀번호는 보안상 의미없는 문자열 삽입 후 disabled 처리

--- pages/user/edit.vue
+++ pages/user/edit.vue
@@ -1,23 +1,43 @@
 <script setup>
-const nicknameInput = { title: '닉네임', placeholder: '유저명을 입력해주세요', readonly: false }
-const emailInput = { title: '아이디 (이메일)', placeholder: '이메일을 입력해주세요', readonly: false }
-const passwordInput = { title: '비밀번호', placeholder: '비밀번호를 입력해주세요', readonly: false, type: 'password' }
-const nameInput = { title: '이름', placeholder: '이름을 입력해주세요', readonly: false }
-const phoneNumberInput = { title: '휴대전화 (선택)', placeholder: '번호를 입력해주세요 (\'-\'포함)', readonly: false }
+import nuxtStorage from "nuxt-storage";
+import constant from "~/service/constant";
+import parseJwt from "~/service/jwtParser";
+import userApi from "~/service/userApi";
+import journeyApi from "~/service/journeyApi";
 
-const userId = useRoute().query.id;
-const user = {
-	id: 1,
-	username: '행복한 닉네임',
-	age: 24,
-	journey: {
-		id: 1,
-		title: '행복한 제주도 올레길 여행'
-	},
-	buddy: {
-		name: '슬탄 AI',
+/* ============================================
+ * UI 컴포넌트용 데이터
+ **/
+const nicknameInput = { title: '닉네임 (필수)', placeholder: '유저명을 입력해주세요', readonly: false }
+const emailInput = { title: '이메일 (필수)', placeholder: '이메일을 입력해주세요', readonly: false }
+const passwordInput = { title: '비밀번호', placeholder: '비밀번호를 입력해주세요', readonly: false, type: 'password' }
+const nameInput = { title: '이름 (필수)', placeholder: '이름을 입력해주세요', readonly: false }
+const phoneNumberInput = { title: '휴대전화 (선택)', placeholder: '번호를 입력해주세요 (\'-\'포함)', readonly: false }
+/* ============================================ */
+
+
+const userDetail = reactive({ info: {}, currentJourney: {} });
+let userTokenFromLocalStorage = reactive({ token: '' });
+onMounted(async () => {
+	userTokenFromLocalStorage.token = nuxtStorage.localStorage.getData(constant.LOCAL_STORAGE_USER_TOKEN_KEY); // user token
+
+	if (userTokenFromLocalStorage.token) {
+		const userToken = userTokenFromLocalStorage.token;
+		const payload = parseJwt(userToken);
+		const userId = payload['user_id'];
+
+		// 유저 상세정보
+		const userDetailResult = await userApi.getUserDetail(userId, userToken);
+		userDetail.info = userDetailResult.user;
+
+		// 유저의 현재 진행중인 여행 정보
+		const currentJourneyResult = await journeyApi.getCurrentUserJourney(userToken);
+		userDetail.currentJourney = currentJourneyResult['journey'];
+	} else {
+		console.log("not login user");
+		navigateTo('/login');
 	}
-}
+});
 
 </script>
 
@@ -29,31 +49,30 @@ const user = {
 
 		<div class="user-infos">
 			<div class="user-profile-image-for-edit">
-				<img src="/images/user/default_user_profile_image.svg" class="user-profile-image" alt="user profile image">
+				<img v-if="userDetail.info['profile_image_url']" :src="userDetail.info['profile_image_url']" class="user-profile-image" alt="user profile image">
+				<img v-else src="/images/user/default_user_profile_image.svg" class="user-profile-image" alt="user default profile image">
 					<img src="/images/user/user_profile_img_edit_btn_icon.svg" class="user-profile-image-edit-btn"
 							 alt="user image edit btn">
 			</div>
-			<span class="user-username">{{ user.username }} 님</span>
+			<span class="user-username">{{ userDetail.info['nickname'] }} 님</span>
 		</div>
 
 		<section class="user-info-edit-page-inputs">
-			<InputView :component="nicknameInput">
+			<InputView :component="nicknameInput" :input-value="userDetail.info['nickname']">
 			</InputView>
-			<InputView :component="emailInput">
+			<InputView :component="emailInput" :input-value="userDetail.info['email']">
 			</InputView>
-			<InputView :component="passwordInput">
+			<InputView :component="passwordInput" :input-value="`**********`" :input-disable="true">
 			</InputView>
-			<InputView :component="nameInput">
+			<InputView :component="nameInput" :input-value="userDetail.info['name']">
 			</InputView>
-			<InputView :component="phoneNumberInput">
+			<InputView :component="phoneNumberInput" :input-value="userDetail.info['phone_number']">
 			</InputView>
 		</section>
 
-			<button class="edit-submit-btn">
-				수정하기
-			</button>
-<!--		<div class="user-info-edit-submit-btn">-->
-<!--		</div>-->
+		<button class="edit-submit-btn">
+			수정하기
+		</button>
 	</section>
 </template>
 
@@ -78,6 +97,12 @@ const user = {
   position: relative;
 }
 
+.user-profile-image {
+	width: 74px;
+	height: 74px;
+	border-radius: 50%;
+}
+
 .user-profile-image-edit-btn {
   position: absolute;
   bottom: 0;
@@ -88,6 +113,11 @@ const user = {
   background-color: #262C31;
 	padding: 3px;
   border-radius: 50%;
+}
+
+.user-profile-image-edit-btn:active {
+	width: 20px;
+	height: 20px;
 }
 
 .user-username {
@@ -115,5 +145,9 @@ const user = {
 	font-size: 16px;
 	font-weight: 600;
 	color: white;
+}
+
+.edit-submit-btn:active {
+	background-color: #528dff;
 }
 </style>
